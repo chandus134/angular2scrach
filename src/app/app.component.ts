d32a8c189c2dee07431704c92327f376c0b8c92e
chandrasekhar-sdn <chandra.sekhar@smartdatainc.net>
2018-04-10 18:35:34 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+//   template: `
+// <p>This is my inline template HTML!</p>
+// `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
